331ee424270bf66f5bb785f80f64d5c2dd27c8e6
poster-keisuke <ta.keisuke4@gmail.com>
2016-11-14 01:07:05 +0900

    [add]mypageの追加
    refs #27 マイページの作成をしました。デザインは現段階のものです。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,21 @@ class UsersController < ApplicationController
 		@nickname = current_user.nickname
 		@articles = current_user.articles.page(params[:page]).per(5).order("created_at DESC")
 	end
+
+	def mypage
+		if User.find_by(nickname: params[:nickname]).present?
+			@user = User.find_by(nickname: params[:nickname])
+			@nickname = params[:nickname]
+			user_article_serch
+			@article_count = @article.count
+		else
+			redirect_to "/"
+		end
+	end
+	private
+
+	def user_article_serch
+		@user = User.find_by(nickname: params[:nickname])
+		@article = @user.articles.page(params[:page]).per(5).order("created_at DESC")
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get '/' => 'articles#index'
   get 'users/:id' => 'users#show'
+  get '/:nickname' => 'users#mypage'
 
   get 'login' => 'login#show'
 
