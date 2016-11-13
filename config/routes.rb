bc0ac3be71a62aba9c86edd6a883ae2058a4b74b
poster-keisuke <ta.keisuke4@gmail.com>
2016-11-14 01:39:23 +0900

    [fix]ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   get '/' => 'articles#index'
   get 'users/:id' => 'users#show'
-  get '/:nickname' => 'users#mypage'
 
   get 'login' => 'login#show'
 
   resources :articles do
   	get "favorites/toggle"
   end
+  get '/:nickname' => 'users#mypage'
 end
