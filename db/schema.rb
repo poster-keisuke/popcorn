59a6ed474770b33ba84f10170fd8a3397481d112
poster-keisuke <ta.keisuke4@gmail.com>
2016-11-16 19:47:00 +0900

    [add]schemaファイルの変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103140958) do
+ActiveRecord::Schema.define(version: 20161116065500) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "text",       limit: 65535
@@ -21,6 +21,27 @@ ActiveRecord::Schema.define(version: 20161103140958) do
     t.integer  "user_id",    limit: 4
     t.string   "title",      limit: 255
   end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "article_id", limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "favorites", ["article_id"], name: "index_favorites_on_article_id", using: :btree
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id", using: :btree
+
+  create_table "supports", force: :cascade do |t|
+    t.integer  "user_id",        limit: 4
+    t.integer  "article_id",     limit: 4
+    t.integer  "support_method", limit: 4, default: 0
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+  end
+
+  add_index "supports", ["article_id"], name: "index_supports_on_article_id", using: :btree
+  add_index "supports", ["user_id"], name: "index_supports_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "",    null: false
@@ -42,4 +63,8 @@ ActiveRecord::Schema.define(version: 20161103140958) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "favorites", "articles"
+  add_foreign_key "favorites", "users"
+  add_foreign_key "supports", "articles"
+  add_foreign_key "supports", "users"
 end
