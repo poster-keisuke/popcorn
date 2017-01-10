a29c1c3161243d67d6217d9328e4db354dc63a2e
poster-keisuke <ta.keisuke4@gmail.com>
2017-01-10 15:04:09 +0900

    rename projects name to popcorn

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_indA_session'
+Rails.application.config.session_store :cookie_store, key: '_popcorn_session'
